8e8912f72ef01aa8fa3a77efedb9c352a44ac976
olegk <kbryzhenyukpc@gmail.com>
2024-11-14 23:24:13 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace siapazon;
+
+        internal class Program
+        {
+             static void Main()
+                {
+
+                Console.WriteLine("Начало диапазона");
+                int startRange = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("конец диапазона");
+                int endRange = int.Parse(Console.ReadLine());
+
+                int a = 0;
+                int b = 1;
+
+                Console.WriteLine("кол-тво чисел Фибоначчи");
+
+                while (a <= endRange)
+                {
+                    if (a >= startRange)
+                    {
+                        Console.Write(a + " ");
+                    }
+
+                    int next = a + b;
+                    a = b;
+                    b = next;
+                }
+            }
+        }
